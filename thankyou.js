7c408062ca9bbdff915447c4a8a5a2f6e8e426c8
Evildeye <137826261+Evildeye@users.noreply.github.com>
2023-11-12 10:51:56 +0700

    Update thankyou.js

--- thankyou.js
+++ thankyou.js
@@ -4,9 +4,9 @@ const noBtn = document.querySelector('.button2');
 const formReact form.getBoundingClientRect();
 const noBtnReact = noBtn.getBoundingClientRect();
 
-noBtn.addEventListener("mouseover', () => {
-	const i - Math.floor(Math.random() * (formReact.width noBtnReact.width)) + 2;
-	const j - Math.floor(Math.random() * (formReact.height noBtnReact.height)) + 2;
+noBtn.addEventListener('mouseover', () => {
+	const i = Math.floor(Math.random() * (formReact.width - noBtnReact.width)) + 2;
+	const j = Math.floor(Math.random() * (formReact.height - noBtnReact.height)) + 2;
 	noBtn.style.left = i + 'px';
 	noBtn.style.top = j + 'px';
 	});
